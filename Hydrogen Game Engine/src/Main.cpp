aaef8aeff93a1ebcb3853ade7b754660b4fbdb9b
salmoncatt <salmoncattyt@gmail.com>
2021-05-12 17:06:33 -0500

    crap theres another bug

--- Hydrogen Game Engine/src/Main.cpp
+++ Hydrogen Game Engine/src/Main.cpp
@@ -66,7 +66,7 @@ public:
 		oxygen.create(oxygenFace);
 
 		GuiText text = GuiText("haha poop", oxygen, Transform2D(Vec2f(100, 100), Vec2f(0), Vec2f(1, 1)));
-		text.create();
+		//text.create();
 
 		while (shouldUpdate() && !Input::getKey(HFR_KEY_ESCAPE)) {
 			ProfileMethod("Main loop");
